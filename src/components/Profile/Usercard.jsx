61f3c96de300977252960fb2ca08ae7aaea6335a
HIMANSHU498 <KUSHWAHHIMANSHU498@GMAIL.COM>
2023-04-19 17:57:26 +0530

    added user profile card

--- src/components/Profile/Usercard.jsx
+++ src/components/Profile/Usercard.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 import "./Usercard.css";
+import userPic from "./../../assets/user-pic.png";
 const Usercard = () => {
-  return <div>Usercard</div>;
+  const userData = JSON.parse(localStorage.getItem("userValues"));
+  const categories = JSON.parse(localStorage.getItem("entertainment Category"));
+
+  return (
+    <>
+      <div className="user-card">
+        <img src={userPic} alt="user-img" className="user-profile" />
+        <div className="card-detail">
+          <div className="card-name">{userData.name}</div>
+          <div className="card-email">{userData.email}</div>
+          <div className="card-username">{userData.username}</div>
+          <div className="category-box">
+            {categories.map((item, i) => {
+              return (
+                <div className="card-category" key={i}>
+                  {item} &nbsp;
+                  <span
+                    style={{
+                      color: "#080C00",
+                      cursor: "pointer",
+                      fontSize: "10px",
+                      marginLeft: "0.6rem",
+                    }}
+                  >
+                    X
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Usercard;
